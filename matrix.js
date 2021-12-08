b9a339f3d28cc77afc141d7021cfe9af4c895483
thalib12 <89063948+thalib12@users.noreply.github.com>
2021-12-08 17:33:00 +0530

    Add files via upload

--- matrix.js
+++ matrix.js
@@ -1,7 +1,14 @@
-var N=4
+var N=5
 matrix=[]
 mrow=[]
 for(i=1;i<=N;i++){
 mrow.push(i)
+matrix.push(mrow)
 }
-console.log(mrow);
+var sum=0
+for(i=0;i<N;i++){
+sum+=matrix[i][i]
+}
+console.log(matrix);
+console.log(sum);
+
